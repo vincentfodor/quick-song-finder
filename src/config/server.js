831841fb20356dce29bd7f5ad905c186860af74e
Vincent Fodor <vincent.fodor@online.de>
2019-05-23 22:14:37 +0200

    Fixed cors origin undefined bug

--- src/config/server.js
+++ src/config/server.js
@@ -7,7 +7,7 @@ export default {
         'http://localhost:3001'
       ];
       
-      if(originWhitelist.indexOf(origin) !== -1) {
+      if(!origin || originWhitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
         callback(new Error(`The origin ${origin} is not allowed to access this data`));
